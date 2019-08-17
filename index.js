535b1a6f6ae4dee8677db155652acf4668d21a46
gregory schrammel <greg@gregs.local>
2019-08-17 01:37:56 -0300

    2

--- index.js
+++ index.js
@@ -10,17 +10,10 @@ function FirebaseUIAuth({ firebase, auth, config, lang }) {
   useEffect(() => {
     if (!loaded) return;
     if (error) throw error;
-<<<<<<< HEAD
     firebase = firebase;
     const firebaseUI = new firebaseui.auth.AuthUI(auth);
     firebaseUI.start(`#${FIREBASEUI_CONTAINER_ID}`, config);
   }, [auth, config, error, firebase, loaded]);
-=======
-    window.firebase = firebase;
-    const firebaseUI = new window.firebaseui.auth.AuthUI(auth);
-    firebaseUI.start(`#${FIREBASEUI_CONTAINER_ID}`, config);
-  }, [error, loaded]);
->>>>>>> a8d132f0c328f842cfd07d305793589c24cd17a3
 
   return (
     <>
