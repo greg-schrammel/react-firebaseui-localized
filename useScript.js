a96f30f6dc6d1be0bb585c0b862fa160f4cb6728
greg schrammel <greg@schrammel.co>
2021-03-03 17:30:10 -0300

    returns state machine

--- useScript.js
+++ useScript.js
@@ -1,32 +1,32 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect, useRef } from "react";
 
 function useScript(src) {
-  const [state, setState] = useState({ loaded: false, error: false });
+  const [state, setState] = useState({ value: "loading", payload: undefined }); // value { loading | loaded | error }
   const script = useRef();
 
   useEffect(() => {
-    script.current = document.createElement('script');
+    script.current = document.createElement("script");
     script.current.src = src;
     script.current.async = true;
 
-    const onScriptLoad = () => setState({ loaded: src, error: false });
+    const onScriptLoad = () => setState({ value: "loaded" });
 
-    const onScriptError = e => {
+    const onScriptError = (e) => {
       script.current.remove();
-      setState({ loaded: false, error: e });
+      setState({ value: "error", payload: e });
     };
 
-    script.current.addEventListener('load', onScriptLoad);
-    script.current.addEventListener('error', onScriptError);
+    script.current.addEventListener("load", onScriptLoad);
+    script.current.addEventListener("error", onScriptError);
 
     document.body.appendChild(script.current);
     return () => {
-      script.current.removeEventListener('load', onScriptLoad);
-      script.current.removeEventListener('error', onScriptError);
+      script.current.removeEventListener("load", onScriptLoad);
+      script.current.removeEventListener("error", onScriptError);
     };
   }, [src]);
 
-  return [state.loaded, state.error];
+  return state;
 }
 
 export default useScript;
